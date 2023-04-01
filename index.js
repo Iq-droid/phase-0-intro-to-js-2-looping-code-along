bed0f215a81f66db34857fb8a2ac211e5691d0cd
Iqra <iqra.ali@student.moringaschool.com>
2023-04-01 10:41:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Ada","Brendan","Aki"];
+const x = "birthday";
+
+function writeCards(names, x){
+    let result = [];
+    for(let i = 0; i < names.length; i++){
+        result.push(`Thank you, ${names[i]}, for the wonderful${x} gift!`);
+    }
+    return result;
+}
+
+let returnedvalue = writeCards(name, x);
+console.log(writeCards(names, x));
+
+function countDown(num){
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
+countDown(6);
